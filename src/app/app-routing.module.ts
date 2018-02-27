3efa03bf3877f17092d68956a0549ddf0e3a3acb
krispex <krispex@gmail.com>
2018-02-26 20:17:51 -0800

    images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent }      from './app.component';
 
+const routes: Routes = [
+  { path: 'test', component: AppComponent }
+];
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
