0c37a0a50b8343cc10a4dbeee438b1c930840eaa
krispex <krispex@gmail.com>
2018-05-31 14:55:53 -0700

    added components and images
    course-detail component
    players componen
    player-detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent }      from './app.component';
 import { CoursesComponent } from './courses/courses.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
 import { FeaturedComponent } from './featured/featured.component';
-
+import { PlayersComponent } from './players/players.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
 
 const routes: Routes = [
   { path: 'courses', component: CoursesComponent },
-  { path: '', component: FeaturedComponent }
+  { path: '', component: FeaturedComponent },
+  { path: 'player_:id', component: PlayerDetailComponent }
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
