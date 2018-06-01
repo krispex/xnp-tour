63ed5edc0fda7b11f6531cc156c6edbfc93c6efb
krispex <krispex@gmail.com>
2018-06-01 11:37:20 -0700

    added serivce, script, and favicon
    added player service
    added player script
    added players script

---
+++ src/app/players.ts
@@ -0,0 +1,8 @@
+import { Player } from './player';
+
+export const PLAYERS: Player[] = [
+  { id: 1, name: 'Casey' },
+  { id: 2, name: 'Carson' },
+  { id: 3, name: 'Geoff' },
+  { id: 4, name: 'James' }
+];
