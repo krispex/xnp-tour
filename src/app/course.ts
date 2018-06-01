2b06f373b63fdd9ffe671c8e32c87e108d9ec941
krispex <krispex@gmail.com>
2018-06-01 12:42:48 -0700

    added courses script and edited rawdata

--- src/app/course.ts
+++ src/app/course.ts
@@ -1,4 +1,5 @@
 export class Course {
     id: number;
     name: string;
+    date: string[];
 }
---
+++ src/app/courses.ts
@@ -0,0 +1,5 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+  {id: 1, name: 'Prince Rupert Pitch & Putt', date: ['2018-02-11']}
+];
