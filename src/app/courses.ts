bb5b4ced6730f266ccd7673f0b8a3bfd9294fbdc
krispex <krispex@gmail.com>
2018-06-10 20:34:09 -0700

    new angular html parsing for courses component

--- src/app/courses.ts
+++ src/app/courses.ts
@@ -2,5 +2,6 @@ import { Course } from './course';
 
 export const COURSES: Course[] = [
   {id: 1, name: 'Prince Rupert Pitch & Putt', date: ['2018-02-11']},
-  {id: 0, name: 'Another Course', date: ['2018-01-01', '2018-01-02']}
+  {id: 2, name: 'Another Course', date: ['2018-02-12', '2018-01-13']},
+  {id: 3, name: 'Course', date: ['2018-02-01', '2018-02-15']}
 ];
