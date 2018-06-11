3650b570e938ad8bd3a1cf2afd1b5d3115f3ab16
krispex <krispex@gmail.com>
2018-06-11 13:53:47 -0700

    courses component sort date table initialization

--- src/app/courses.ts
+++ src/app/courses.ts
@@ -2,6 +2,6 @@ import { Course } from './course';
 
 export const COURSES: Course[] = [
   {id: 1, name: 'Prince Rupert Pitch & Putt', date: ['2018-02-11']},
-  {id: 2, name: 'Another Course', date: ['2018-02-12', '2018-01-13']},
-  {id: 3, name: 'Course', date: ['2018-02-01', '2018-02-15']}
+  {id: 2, name: 'Another Course', date: ['2018-02-12', '2018-02-15']},
+  {id: 3, name: 'Course Course Course', date: ['2018-02-14', '2018-02-14']}
 ];
