7e2d3a94ddcc3ad9e069446442b1ff9c5a9fd78d
krispex <krispex@gmail.com>
2018-06-12 16:10:46 -0700

    added image to course 1, added to featured component

--- src/app/courses.ts
+++ src/app/courses.ts
@@ -1,7 +1,7 @@
 import { Course } from './course';
 
 export const COURSES: Course[] = [
-  {id: 1, name: 'Prince Rupert Pitch & Putt', date: ['2018-02-11']},
+  {id: 1, name: 'Rupert Park Pitch & Putt', date: ['2018-02-11']},
   {id: 2, name: 'Another Course', date: ['2018-02-12', '2018-02-15']},
   {id: 3, name: 'Course Course Course', date: ['2018-02-14', '2018-02-14']}
 ];
