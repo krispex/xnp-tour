93d445f7676c585e563d7f98b595ee0ead9bd2f2
krispex <krispex@gmail.com>
2018-07-06 18:08:29 -0700

    added score.ts and scores.ts
    added rawPlayerData from data.json to scores.ts

---
+++ src/app/score.ts
@@ -0,0 +1,23 @@
+export class Score {
+    id: string;
+    name: string;
+    date: string;
+    hole1: string;
+    hole2: string;
+    hole3: string;
+    hole4: string;
+    hole5: string;
+    hole6: string;
+    hole7: string;
+    hole8: string;
+    hole9: string;
+    hole10: string;
+    hole11: string;
+    hole12: string;
+    hole13: string;
+    hole14: string;
+    hole15: string;
+    hole16: string;
+    hole17: string;
+    hole18: string;
+}
