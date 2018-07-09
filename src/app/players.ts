e1244bd7550a165df569ebe09b1550f2d4202678
krispex <krispex@gmail.com>
2018-07-08 22:52:05 -0700

    build and switched player id 1 and 2 to correct ways

--- src/app/players.ts
+++ src/app/players.ts
@@ -1,8 +1,8 @@
 import { Player } from './player';
 
 export const PLAYERS: Player[] = [
-  { id: 1, name: 'Casey' },
-  { id: 2, name: 'Carson' },
+  { id: 1, name: 'Carson' },
+  { id: 2, name: 'Casey' },
   { id: 3, name: 'Geoff' },
   { id: 4, name: 'James' }
 ];
